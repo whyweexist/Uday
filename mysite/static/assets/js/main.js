2b747bc2379824849b5f2bd128e4883a34a99b3b
whyweexist <167321903+whyweexist@users.noreply.github.com>
2024-08-01 20:58:19 +0530

    Update main.js

--- mysite/static/assets/js/main.js
+++ mysite/static/assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Mentor
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function () {
   "use strict";
 
